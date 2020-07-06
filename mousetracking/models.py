cbfeedd71c8ed73994eb86fe6b08038e2c04eca0
Ty Hayes <tyson.hayes@wbs.ac.uk>
2020-07-06 13:21:31 +0100

    abstract the base models

--- mousetracking/models.py
+++ mousetracking/models.py
@@ -2,7 +2,9 @@ from otree.api import (
     models, widgets, BaseConstants, BaseSubsession, BaseGroup, BasePlayer, ExtraModel,
     Currency as c, currency_range
 )
-from django.db.models import BigIntegerField
+
+from .mousetracking import MouseTrackingGroup, MouseTrackingPlayer, MouseEvent, custom_export
+
 author = 'Your name here'
 
 doc = """
@@ -22,32 +24,9 @@ class Subsession(BaseSubsession):
             p.stimulus = "chicken kiev"
 
 
-class Group(BaseGroup):
-    def live_event(self, id_in_group, payload):
-        player = self.get_player_by_id(id_in_group)
-        timestamp = payload['timestamp']
-        region = payload['region']
-        action = payload['action']
-        mouse_event = MouseEvent.objects.create(player=player, timestamp=timestamp, region=region, action=action)
-        mouse_event.save()
-        #return {id_in_group: "created mouse_event %d" % mouse_event.id}
+class Group(MouseTrackingGroup):
+    pass
     
 
-class Player(BasePlayer):
+class Player(MouseTrackingPlayer):
     stimulus = models.StringField()
-    def mouse_events(self):
-        return MouseEvent.objects.filter(player=self)
-
-
-class MouseEvent(ExtraModel):
-    player = models.Link(Player)
-    region = models.IntegerField()
-    action = models.StringField()
-    timestamp = BigIntegerField()
-
-
-def custom_export(players):
-    yield ['session','participant_code','stimulus','region','action', 'timestamp']
-    for p in players:
-        for m in p.mouse_events():
-            yield [p.session.code, p.participant.code, p.stimulus, m.region, m.action, m.timestamp]
---
+++ mousetracking/mousetracking.py
@@ -0,0 +1,41 @@
+from otree.api import (
+    models, widgets, BaseConstants, BaseSubsession, BaseGroup, BasePlayer, ExtraModel,
+    Currency as c, currency_range
+)
+from django.db.models import BigIntegerField
+#from django.
+
+class MouseTrackingGroup(BaseGroup):
+    def live_event(self, id_in_group, payload):
+        player = self.get_player_by_id(id_in_group)
+        timestamp = payload['timestamp']
+        region = payload['region']
+        action = payload['action']
+        mouse_event = MouseEvent.objects.create(player=player, timestamp=timestamp, region=region, action=action)
+        mouse_event.save()
+
+    class Meta:
+        abstract=True
+
+
+class MouseTrackingPlayer(BasePlayer):
+    def mouse_events(self):
+        return MouseEvent.objects.filter(player=self)
+
+    class Meta:
+        abstract=True
+
+class MouseEvent(ExtraModel):
+    #player = models.Link(MouseTrackingPlayer)
+    #player = ForeignKey('mousetracking.Player')
+    player = models.Link('mousetracking.Player')
+    region = models.IntegerField()
+    action = models.StringField()
+    timestamp = BigIntegerField()
+
+
+def custom_export(players):
+    yield ['session','participant_code','stimulus','region','action', 'timestamp']
+    for p in players:
+        for m in p.mouse_events():
+            yield [p.session.code, p.participant.code, p.stimulus, m.region, m.action, m.timestamp]
